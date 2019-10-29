2235b9a858d8d589dd2d85ead689ac1651d4dc2d
Peter Shumie <shumie.peter@gmail.com>
2019-10-28 19:59:18 -0500

    added log function for log.txt file

--- liri.js
+++ liri.js
@@ -14,3 +14,27 @@ var theBand = '';
 var fileName = 'log.txt';
 var fullCommand = [];
 
+
+for (var i = 3; i < arg.length; i++) {
+  refrence.push(arg[i])
+}
+
+var refrenceBand = refrence.join("");
+
+// fullCommand logs commands to to log.txt file after command input
+fullCommand.push(command);
+if(refrence.length != 0){
+  fullCommand.push(refrenceBand);
+}
+
+
+//Function that logs information to  log.txt file using fs.append element
+function log(value){
+  fs.appendFile(fileName, ',' + value, function(err){
+    return console.log("Ya blew it")
+  })
+}
+
+log(fullCommand);
+
+// Command statements for BandInTown, Spotify, and OMDB
